2ffde160b86c5391ece0472cf1ab266fc97ed8b4
SanketJagadale <sanket122000jagadale@gmail.com>
2024-09-29 01:47:47 +0530

    html changes done

--- catalyst_count_main/urls.py
+++ catalyst_count_main/urls.py
@@ -16,14 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-from allauth.account import views as allauth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.Home,name="Home"),
-    # path('',allauth_views.login, name="account_login"),
-    # path('accounts/signup/', allauth_views.signup, name="account_signup"),
-    # path('accounts/login/', allauth_views.login, name="account_login"),
-    # path('accounts/logout/', allauth_views.logout, name="account_logout"),
     path('accounts/', include('allauth.urls')),
 ]
--- catalyst_count_main/views.py
+++ catalyst_count_main/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def Home(request):
-    templatefilename="layout.html"
+    templatefilename="home.html"
     return render(request,templatefilename)
